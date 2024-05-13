d27b1d82070769249a881263a17a413e0c2b09a5
tudda <tentufancr7@gmail.com>
2024-05-13 22:20:41 +0700

    .

--- src/main/java/com/fpt/adminservice/auth/controller/ManagerCompanyController.java
+++ src/main/java/com/fpt/adminservice/auth/controller/ManagerCompanyController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
-@RequestMapping("/manager/companyd")
+@RequestMapping("/manager/company")
 @RequiredArgsConstructor
 public class ManagerCompanyController {
     private final UserService userService;
