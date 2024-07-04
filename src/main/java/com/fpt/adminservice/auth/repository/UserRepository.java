a15cc07a11d78009bc1626d486025145b4b52c52
upcode <nguyenhainammoonligh@gmail.com>
2024-07-04 09:49:46 +0700

    upcode

--- src/main/java/com/fpt/adminservice/auth/repository/UserRepository.java
+++ src/main/java/com/fpt/adminservice/auth/repository/UserRepository.java
@@ -10,6 +10,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
@@ -34,4 +35,13 @@ public interface UserRepository extends JpaRepository<User, Integer> {
                                @Param("fromDate") LocalDateTime fromDate,
                                @Param("toDate") LocalDateTime toDate,
                                Pageable pageable);
+
+
+
+    @Query(value = """
+        SELECT email, end_date_use_service
+            FROM users
+            WHERE end_date_use_service <= CURRENT_DATE + INTERVAL '7' DAY;
+    """, nativeQuery = true)
+    List<UserInterface> getEndUserServiceLessTime();
 }
--- src/main/java/com/fpt/adminservice/config/ScheduledTasksConfig.java
+++ src/main/java/com/fpt/adminservice/config/ScheduledTasksConfig.java
@@ -1,20 +1,29 @@
 package com.fpt.adminservice.config;
 
+import com.fpt.adminservice.admin.dto.UserInterface;
+import com.fpt.adminservice.auth.repository.UserRepository;
 import jakarta.mail.MessagingException;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static com.fpt.adminservice.utils.Constants.MESSAGE_MAIL.EXPIRED_PACKAGE;
 
 @Component
+@RequiredArgsConstructor
 public class ScheduledTasksConfig {
-    @Autowired
-    private MailService mailService;
-    @Scheduled(cron = "0 10 15 * * ?")
+    private final MailService mailService;
+    private final UserRepository userRepository;
+
+    @Scheduled(fixedRate = 5000)
     public void scheduleTaskUsingCronExpression() throws MessagingException {
         // function get email và ngày hết hạn dịch vụ check còn 7 ngày thì gửi mail cảnh báo
-        String[] emailListTo = new String[]{"tuddaallt@gmail.com"};
+        List<UserInterface> users = userRepository.getEndUserServiceLessTime();
+        String[] emailListTo = users.stream().map(e -> e.getEmail()).toArray(String[]::new);
         mailService.sendNewMail(emailListTo, null,"" , EXPIRED_PACKAGE, null);
     }
 }
